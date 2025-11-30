fcb1f134b0dbd5f6521b75c538c3a041bffdd3fd
HakoniwaStudio <ogata.ad@gmail.com>
2025-11-30 10:32:58 +0900

    Create __init__.py

---
+++ app/backend/core/__init__.py
@@ -0,0 +1,6 @@
+"""Core Package"""
+
+from backend.core.base_module import BaseModule
+from backend.core.module_manager import ModuleManager, module_manager
+
+__all__ = ["BaseModule", "ModuleManager", "module_manager"]
